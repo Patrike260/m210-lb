060f1db3924e53cf39e39a612552cafe31f47ad2
Patrike260 <Patrik.e260@gmail.com>
2024-12-13 09:55:15 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# M210-LB Medienbibliotek
+
+## UseCase
+
+1. User kann sich registrieren/Anmelen 
+2. User kann seine bibliothek anschauen
+3. user kann neue medien hinzufügen
+4. User kann nach kategorie umschalten
---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+
+import './App.css'
+import { useState, useEffect } from 'react'
+import { createClient } from '@supabase/supabase-js'
+import { Auth } from '@supabase/auth-ui-react'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
+
+const supabase = createClient('https://kfcubxjlzhibqkgpdlsg.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtmY3VieGpsemhpYnFrZ3BkbHNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQwMDgxNjcsImV4cCI6MjA0OTU4NDE2N30.8Cp1ZKuBi740SuyJIVYrVNodQu5p_kmn669a0qYp7PM')
+
+export default function App() {
+  const [session, setSession] = useState(null)
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+
+    return () => subscription.unsubscribe()
+  }, [])
+
+  if (!session) {
+    return (<Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />)
+  }
+  else {
+    return (<div>Logged in!</div>)
+  }
+}
